eb3db091ca578299970804fc5d7e6cda6139c193
CaliFost <35895111+CaliFost@users.noreply.github.com>
2020-04-17 17:18:51 -0300

    Update Hello World.py

--- Hello World.py
+++ Hello World.py
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-print("Probando git con MaykoRS")
-=======
-print("Probando git con Paula")
->>>>>>> dc4e9eb1641884e760257e95efc923bc8443792b
-=======
-print("Probando git con Horu98")
->>>>>>> 3f5b74df67ad6d0e2a28cd9d99f4d110fd2e7692
+print("Probando git con MaykoRS y Paula y Horu98")
